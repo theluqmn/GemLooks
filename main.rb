b0f1dca23d2241b52cde70128d1f7265631e8cdb
luq <luqman.yazrie@outlook.com>
2024-05-01 16:53:39 +0800

    linear search and binary search now working

--- main.rb
+++ main.rb
@@ -1,27 +1,74 @@
+require 'time'
+
 # Linear search function
-def linear_search(list, item, keep_search)
-    found = false
-    index = 0
+def linear_search(array, target)
+    # Start the timer
+    start_time = Time.now
 
-    # Loops through the provided list
-    for num in list
-        index = index + 1
+    # Loops through the array
+    array.each_with_index do |element, index|
+        # Puts out where the target is found
+        if element == target
+            # Measure the time it took for the search
+            end_time = Time.now
+            # Calculate the time it took for the search
+            time_taken = end_time - start_time
 
-        # Check if num is number
-        if num == item
-            puts "Found #{item} at index #{index}"
+            puts "Target #{target} found at index #{index}\nTime taken: #{time_taken} seconds"
             return index
-            found = true
-            if keep_search == false
-                break
-            end
         end
     end
 
-    # If item isnt found
-    if found == false
-        puts "Couldn't find #{item} in list"
-    end
+    # Measure the time it took for the search
+    end_time = Time.now
+    # Calculate the time it took for the search
+    time_taken = end_time - start_time
+
+    puts "Target #{target} not found\nTime taken: #{time_taken} seconds"
+    return nil
 end
 
-linear_search([10, 5, 8, 15, 0, 20, 10], 10, true)
+# Binary search function
+def binary_search(array, target)
+    # Start the timer
+    start_time = Time.now
+
+    # Sets the low and high indexes
+    low = 0
+    high = array.length - 1
+
+    while low <= high
+        # Finds the middle index
+        mid = (low + high) / 2
+        guess = array[mid]
+
+        # Checks if the guess is the target
+        if guess == target
+            # Measure the time it took for the search
+            end_time = Time.now
+            # Calculate the time it took for the search
+            time_taken = end_time - start_time
+
+            puts "Target #{target} found at index #{mid}\nTime taken: #{time_taken} seconds"
+            return mid
+        
+        # Checks if the guess is too high or too low
+        elsif guess > target
+            high = mid - 1
+        
+        else
+            low = mid + 1
+        end
+    end
+
+    # Measure the time it took for the search
+    end_time = Time.now
+    # Calculate the time it took for the search
+    time_taken = end_time - start_time
+
+    puts "Target #{target} not found\nTime taken: #{time_taken} seconds"
+    return nil
+end
+
+binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7)
+linear_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9)
